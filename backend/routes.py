83c6e05da5ee432bce8754487b4dc740107d978a
sapna maurya <138869250+sapnamaurya@users.noreply.github.com>
2025-05-06 11:51:10 +0530

    sss

--- backend/routes.py
+++ backend/routes.py
@@ -12,3 +12,9 @@ def insights():
 @api.route('/test', methods=['GET'])
 def test():
     return jsonify({"message": "Server is running"})
+def insights():
+    print("Root route accessed")
+    data = generate_insights()
+    print("Generated data:", data)
+    print("Type:", type(data))
+    return jsonify(data)
--- backend/routesHandling/routes.js
+++ backend/routesHandling/routes.js
@@ -1,40 +1,55 @@
-import { Router } from 'express';
+import { Router } from "express";
 const router = Router();
 
-import{ 
-    addExpense, 
-    getAllExpenses, 
-    getExpenseById, 
-    updateExpense, 
-    deleteExpense, 
-    getExpensesByCategory, 
-} from './controllers/expense.js';
-import{ 
-    addIncome, 
-    getIncomes, 
-    getIncomeById, 
-    updateIncome, 
-    deleteIncome 
-} from './controllers/income.js';
-import { registerUser, loginUser } from './controllers/authorization.js';
+import {
+  addExpense,
+  getAllExpenses,
+  getExpenseById,
+  updateExpense,
+  deleteExpense,
+  getExpensesByCategory,
+} from "./controllers/expense.js";
+import {
+  addIncome,
+  getIncomes,
+  getIncomeById,
+  updateIncome,
+  deleteIncome,
+} from "./controllers/income.js";
+import { registerUser, loginUser } from "./controllers/authorization.js";
 
 //Expense Routes
-router.post('/expenses', addExpense);
-router.get('/expenses', getAllExpenses);
-router.get('/expenses/category', getExpensesByCategory); 
-router.get('/expenses/:id', getExpenseById); 
-router.put('/expenses/:id', updateExpense); 
-router.delete('/expenses/:id', deleteExpense);
+router.post("/expenses", addExpense);
+router.get("/expenses", getAllExpenses);
+router.get("/expenses/category", getExpensesByCategory);
+router.get("/expenses/:id", getExpenseById);
+router.put("/expenses/:id", updateExpense);
+router.delete("/expenses/:id", deleteExpense);
 
 // Income Routes
-router.post('/incomes', addIncome);
-router.get('/incomes', getIncomes);
-router.get('/incomes/:id', getIncomeById); 
-router.put('/incomes/:id', updateIncome); 
-router.delete('/incomes/:id', deleteIncome);
+router.post("/incomes", addIncome);
+router.get("/incomes", getIncomes);
+router.get("/incomes/:id", getIncomeById);
+router.put("/incomes/:id", updateIncome);
+router.delete("/incomes/:id", deleteIncome);
 
 // Authentication Routes
-router.post('/auth/register', registerUser);
-router.post('/auth/login', loginUser);
+router.post("/auth/register", registerUser);
+router.post("/auth/login", loginUser);
+
+router.get("/dashboard", (req, res) => {
+  res.json({
+    categories: {
+      Food: 200,
+      Transport: 150,
+      Entertainment: 300,
+    },
+    monthly: {
+      January: 1200,
+      February: 1000,
+      March: 900,
+    },
+  });
+});
 
 export default router;
